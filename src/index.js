e5d6e2a261db6c7b80b61254a2ad56c71b02caa5
Paweł Benkowski <pawlo97.pb@gmail.com>
2022-06-23 13:22:47 +0200

    #09.134 The of and from opertaors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-import { fromEvent } from 'rxjs';
+import { from } from 'rxjs';
 
-const observable = fromEvent(
-    document,
-    "click"
-)
+const observable = from(fetch('https://jsonplaceholder.typicode.com/todos/1'))
 
-observable.subscribe(console.log)
+observable.subscribe({
+    next: (value) => {
+        console.log(value)
+    },
+    complete: () => {
+        console.log('of completes after iteration')
+    }
+})
+
+console.log('hello');
